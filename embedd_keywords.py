80d5ab6a1134f2fa2bb0557a82dbc2cac7222325
Mateusz Wojciechowski <120648222+Mateusz-Wojciechowski@users.noreply.github.com>
2024-05-12 19:08:51 +0200

    update

--- embedd_keywords.py
+++ embedd_keywords.py
@@ -1,24 +1,57 @@
 from transformers import AutoTokenizer, AutoModel
 import pandas as pd
 import json
+import torch
+import pickle
+
+if torch.cuda.is_available():
+    device = torch.device("cuda")
+    print(f"CUDA is available. Device name: {torch.cuda.get_device_name(0)}")
+else:
+    device = torch.device("cpu")
+    print("CUDA is not available, using CPU instead.")
 
 keywords_df = pd.read_csv('keywords_merged.csv')
 tokenizer = AutoTokenizer.from_pretrained('bert-base-uncased')
 model = AutoModel.from_pretrained('bert-base-uncased')
 
+model = model.to(device)
+
 
 def get_bert_embedding(text):
-    inputs = tokenizer(text, return_tensors="pt", padding=True, truncation=True)
+    inputs = tokenizer(text, return_tensors="pt", padding=True, truncation=True).to(device)
     outputs = model(**inputs)
-    return outputs.last_hidden_state.mean(dim=1).detach().numpy()
+    return outputs.last_hidden_state.mean(dim=1).cpu().detach().numpy()
 
 
 keywords_df['BertKeyWords'] = keywords_df['BertKeyWords'].apply(json.loads)
 keywords_df['YakeKeyWords'] = keywords_df['YakeKeyWords'].apply(json.loads)
 keywords_df['TextRankKeywords'] = keywords_df['TextRankKeywords'].apply(json.loads)
 
-keywords_df['BertKeyWords_embeddings'] = keywords_df['BertKeyWords'].apply(lambda x: [get_bert_embedding(word) for word in x])
-keywords_df['YakeKeyWords_embeddings'] = keywords_df['YakeKeyWords'].apply(lambda x: [get_bert_embedding(word) for word in x])
-keywords_df['TextRankKeywords_embeddings'] = keywords_df['TextRankKeywords'].apply(lambda x: [get_bert_embedding(word) for word in x])
+bert_embeddings = []
+yake_embeddings = []
+textrank_embeddings = []
 
-print(keywords_df['BertKeyWords_embeddings'])
+for i in range(len(keywords_df)):
+    print(i)
+    bert, yake, textrank = [], [], []
+    for word in keywords_df['BertKeyWords'][i]:
+        bert.append(get_bert_embedding(word))
+    for word in keywords_df['YakeKeyWords'][i]:
+        yake.append(get_bert_embedding(word))
+    for word in keywords_df['TextRankKeywords'][i]:
+        textrank.append(get_bert_embedding(word))
+
+    bert_embeddings.append(bert)
+    yake_embeddings.append(yake)
+    textrank_embeddings.append(textrank)
+
+
+with open('bert_embeddings.pkl', 'wb') as file:
+    pickle.dump(bert_embeddings, file)
+
+with open('yake_embeddings.pkl', 'wb') as file:
+    pickle.dump(yake_embeddings, file)
+
+with open('textrank_embeddings.pkl', 'wb') as file:
+    pickle.dump(textrank_embeddings, file)
