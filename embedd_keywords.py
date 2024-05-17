003e81cb1e311950268d131832d65b063ed15dde
Mateusz Wojciechowski <120648222+Mateusz-Wojciechowski@users.noreply.github.com>
2024-05-17 20:05:25 +0200

    update

--- embedd_keywords.py
+++ embedd_keywords.py
@@ -27,24 +27,28 @@ def get_bert_embedding(text):
 keywords_df['BertKeyWords'] = keywords_df['BertKeyWords'].apply(json.loads)
 keywords_df['YakeKeyWords'] = keywords_df['YakeKeyWords'].apply(json.loads)
 keywords_df['TextRankKeywords'] = keywords_df['TextRankKeywords'].apply(json.loads)
+keywords_df['RakeKeyWords'] = keywords_df['TextRankKeywords'].apply(json.loads)
 
 bert_embeddings = []
 yake_embeddings = []
 textrank_embeddings = []
+rake_embeddings = []
 
 for i in range(len(keywords_df)):
-    print(i)
-    bert, yake, textrank = [], [], []
+    bert, yake, textrank, rake = [], [], [], []
     for word in keywords_df['BertKeyWords'][i]:
         bert.append(get_bert_embedding(word))
     for word in keywords_df['YakeKeyWords'][i]:
         yake.append(get_bert_embedding(word))
     for word in keywords_df['TextRankKeywords'][i]:
         textrank.append(get_bert_embedding(word))
+    for word in keywords_df['RakeKeyWords'][i]:
+        rake.append(get_bert_embedding(word))
 
     bert_embeddings.append(bert)
     yake_embeddings.append(yake)
     textrank_embeddings.append(textrank)
+    rake_embeddings.append(rake)
 
 
 with open('bert_embeddings.pkl', 'wb') as file:
@@ -54,4 +58,7 @@ with open('yake_embeddings.pkl', 'wb') as file:
     pickle.dump(yake_embeddings, file)
 
 with open('textrank_embeddings.pkl', 'wb') as file:
+    pickle.dump(textrank_embeddings, file)
+
+with open('rake_embeddings.pkl', 'wb') as file:
     pickle.dump(textrank_embeddings, file)
--- keyword_data_preparation.py
+++ keyword_data_preparation.py
@@ -13,16 +13,19 @@ def safely_eval_literal(column):
 keywords_bert = pd.read_csv('keywords_bert.csv', index_col=None)
 keywords_yake = pd.read_csv('keywords_yake.csv', index_col=None)
 keywords_textrank = pd.read_csv('keywords_textrank.csv', index_col=None)
+keywords_rake = pd.read_csv('keywords_rake.csv', index_col=None)
 
-keywords_df = pd.concat([keywords_bert, keywords_yake, keywords_textrank], axis=1)
+keywords_df = pd.concat([keywords_bert, keywords_yake, keywords_textrank, keywords_rake], axis=1)
 
 keywords_df['BertKeyWords'] = keywords_df['BertKeyWords'].apply(safely_eval_literal)
 keywords_df['YakeKeyWords'] = keywords_df['YakeKeyWords'].apply(safely_eval_literal)
 keywords_df['TextRankKeywords'] = keywords_df['TextRankKeywords'].apply(safely_eval_literal)
+keywords_df['RakeKeyWords'] = keywords_df['RakeKeyWords'].apply(safely_eval_literal)
 
 keywords_df['BertKeyWords'] = keywords_df['BertKeyWords'].apply(json.dumps)
 keywords_df['YakeKeyWords'] = keywords_df['YakeKeyWords'].apply(json.dumps)
 keywords_df['TextRankKeywords'] = keywords_df['TextRankKeywords'].apply(json.dumps)
+keywords_df['RakeKeyWords'] = keywords_df['RakeKeyWords'].apply(json.dumps)
 
 keywords_df.to_csv('keywords_merged.csv', index=False)
 
